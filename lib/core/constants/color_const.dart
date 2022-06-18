ab05f2b63360b5ab205b0139352570859048ac43
Nodirbek Rasuljonov <nodirbekmaqsudjonovich@gmail.com>
2022-06-18 10:19:39 +0500

    and other changes

--- lib/core/constants/color_const.dart
+++ lib/core/constants/color_const.dart
@@ -2,4 +2,7 @@ import 'package:flutter/material.dart';
 
 class ColorConst{
   static Color kPrimaryColor=Color(0xff0071ff);
+  static Color transparent=Colors.transparent;
+  static Color lighttext=Colors.black;
+  static Color darktext=Colors.white;
 }
